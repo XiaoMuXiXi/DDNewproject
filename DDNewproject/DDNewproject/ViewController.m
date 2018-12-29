85d3f34ffd9364f397ef41ff60c21a0962f6fb2d
XiaoMuDanDan <770228653@qq.com>
2018-12-29 10:35:50 +0800

    init

---
+++ DDNewproject/DDNewproject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DDNewproject
+//
+//  Created by DDCJ_XiaoZhu on 2018/12/29.
+//  Copyright © 2018 DDCJ_XiaoZhu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
